9857e634df36619b30c4154ade8892e75a960f3f
Natalia-dot <nataliadurantegarcia@gmail.com>
2023-11-28 18:06:32 +0100

    Login implementado y funcionando y rutas condicionales creadas

---
+++ src/hooks/useErrorLogin.jsx
@@ -0,0 +1,64 @@
+import Swal from "sweetalert2";
+
+export const useErrorLogin = (res, setRes, login, setSuccessfulLogin) => {
+console.log(res, "Respuesta en useLoginError")
+    if (res?.status == 200) {
+        const updatedUser = {
+          token: res.data.token,
+          name: res.data.user.name,
+          userEmail: res.data.user.userEmail,
+          image: res.data.user.image,
+          isVerified: res.data.user.isVerified,
+          _id: res.data.user._id,
+          role: res.data.user.role
+        };
+        console.log(updatedUser)
+    
+        const userString = JSON.stringify(updatedUser);
+        login(userString);
+        setSuccessfulLogin(() => true);
+    
+        Swal.fire({
+          icon: "success",
+          title: "Welcome back to DAMtabase",
+          text: "Succesfully logged in.",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    
+    
+      if (res?.response?.data?.includes("User not found.")) {
+        setRes(() => ({}));
+        Swal.fire({
+          icon: "error",
+          title: "Sorry!",
+          text: "This user is not registered! Try making an account, it's super easy!",
+          showConfirmButton: false,
+          timer: 3500,
+        });
+      }
+    
+    
+      if (res?.response?.data?.includes("Password is incorrect.")) {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Password is incorrect.",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        setRes(() => ({})); //tip acceder al estado actualizado con una callback
+      }
+    
+      if (res?.response?.status == 500) {
+        Swal.fire({
+          icon: "error",
+          title: "Sorry!",
+          text: "We had trouble with the server. Please try again later.",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        setRes(() => ({})); //tip acceder al estado actualizado con una callback!!
+      }
+}
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,104 @@
-import './Login.css'
+import { useForm } from "react-hook-form";
+import "./Login.css";
+import { useEffect, useState } from "react";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import { loginUser } from "../../services/user.service";
+import { useErrorLogin } from "../../hooks/useErrorLogin";
+import { useAuth } from "../../context/authContext";
 
 export const Login = () => {
+  const { register, handleSubmit } = useForm();
+  const { login, setUser } = useAuth();
+  const navigate = useNavigate();
+
+  const [isSent, setIsSent] = useState(false); //isSent maneja los botones, el disable
+  const [res, setRes] = useState({});
+  const [successfulLogin, setSuccessfulLogin] = useState(false);
+
+  const formSubmit = async (formData) => {
+    setIsSent(true);
+    setRes(await loginUser(formData));
+    setIsSent(false);
+  };
+
+  useEffect(() => {
+    console.log("res en el primer useEffect", res);
+    useErrorLogin(res, setRes, login, setSuccessfulLogin);
+  }, [res]);
+
+  useEffect(() => {
+    setUser(() => null);
+    localStorage.removeItem("user");
+  }, []);
+
+  if (successfulLogin) {
+    console.log("succesfullogin");
+    if (res?.data?.user?.check == false) {
+      return <Navigate to="/verifyCode" />;
+    } else {
+      switch (res.data.user.interestedIn) {
+        case "motogp":
+          <Navigate to="/motogp" />;
+          break;
+        case "powerlifting":
+          <Navigate to="/powerlifting" />;
+          break;
+        case "fifa":
+          <Navigate to="/fifa" />;
+          break;
+        default:
+          <Navigate to="/"/>
+          break;
+      }
+    }
+  }
+
   return (
-    <div>Login</div>
-  )
-}
+    <>
+      <div className="formContainer">
+        <form onSubmit={handleSubmit(formSubmit)}>
+          <input
+            type="email"
+            id="userEmail"
+            name="userEmail"
+            autoComplete="false"
+            {...register("email", { required: true })}
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            autoComplete="false"
+            {...register("password", { required: true })}
+          />
+          <label htmlFor="password">Password</label>
+          <div className="buttonContainer">
+            <button
+              type="submit"
+              disabled={isSent}
+              style={{
+                background: isSent ? "color-boton" : "color-boton-des",
+              }}
+            >
+              Login
+            </button>
+          </div>
+          <p className="bottom-text">
+            <small>
+              Have you forgotten the password?
+              <Link to="/forgotPassword" className="anchorCustom">
+                Change password
+              </Link>
+            </small>
+          </p>
+        </form>
+        <div className="footerForm">
+          <p className="parrafoLogin">
+            Are you not registered? <Link to="/register">Register Here</Link>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
