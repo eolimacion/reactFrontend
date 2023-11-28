d353e79faf088a51812769560e27998cc8d7f249
Alsamu92 <alexsanzm@email.com>
2023-11-28 15:37:50 +0100

    json

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,6 +1,138 @@
-import './Register.css'
+import "./Register.css";
+
+
+import { useForm } from "react-hook-form";
+import { Link, Navigate } from "react-router-dom";
+import { useAuth } from "../../context/authContext";
+// import { registerUser } from "../../services/user.service";
+import { useEffect, useState } from "react";
+// import { useErrorRegister } from "../../hooks/useErrorRegister";
+
+
 export const Register = () => {
+  // allUser es la respuesta completa del 200 del service de registro
+  const { allUser, setAllUser, bridgeData } = useAuth();
+  const { register, handleSubmit } = useForm();
+  const [res, setRes] = useState({});
+  const [send, setSend] = useState(false);
+  const [okRegister, setRegisterOk] = useState(false);
+
+
+  const formSubmit = async (formData) => {
+const customFormData = {
+        ...formData,
+      };
+
+      setSend(true);
+      setRes(await registerUser(customFormData));
+      setSend(false);
+    
+  };
+
+
+  
+
+  if (okRegister) {
+    //si todo esta ok navega a la pagina del codigo
+    return <Navigate to="/verifyCode" />;
+  }
   return (
-    <div>Register</div>
-  )
-}
+    <div className="todo-form">
+      <div className="form-wrap">
+        <h1>Sign Up</h1>
+        <p>Champion Within, Victories Begin</p>
+        <form onSubmit={handleSubmit(formSubmit)}>
+          <div className="user_container form-group">
+            <input
+              className="input_user"
+              type="text"
+              id="name"
+              name="name"
+              autoComplete="false"
+              {...register("name", { required: true })}
+            />
+            <label htmlFor="custom-input" className="custom-placeholder">
+              Username
+            </label>
+          </div>
+          <div className="password_container form-group">
+            <input
+              className="input_user"
+              type="password"
+              id="password"
+              name="password"
+              autoComplete="false"
+              {...register("password", { required: true })}
+            />
+            <label htmlFor="custom-input" className="custom-placeholder">
+              Password
+            </label>
+          </div>
+
+          <div className="email_container form-group">
+            <input
+              className="input_user"
+              type="email"
+              id="email"
+              name="email"
+              autoComplete="false"
+              {...register("email", { required: true })}
+            />
+            <label htmlFor="custom-input" className="custom-placeholder">
+              E-mail
+            </label>
+
+            <div className="sexo">
+              <input
+                type="radio"
+                name="sexo"
+                id="hombre"
+                value="hombre"
+                {...register("gender")}
+              />
+              
+              <label htmlFor="hombre" className="label-radio hombre">
+                Hombre
+              </label>
+              <input
+                type="radio"
+                name="sexo"
+                id="mujer"
+                value="mujer"
+                {...register("gender")}
+              />
+              <label htmlFor="mujer" className="label-radio mujer">
+                Mujer
+              </label>
+            </div>
+
+          
+          </div>
+
+          <div className="btn_container">
+            <button
+              className="btn"
+              type="submit"
+              disabled={send}
+              style={{ background: send ? "#49c1a388" : "#2f7a67" }}
+            >
+              Register
+            </button>
+          </div>
+          <p className="bottom-text">
+            <small>
+              By clicking the Sign Up button, you agree to our{" "}
+              <Link className="anchorCustom">Terms & Conditions</Link> and{" "}
+              <Link className="anchorCustom">Privacy Policy</Link>.
+            </small>
+          </p>
+        </form>
+      </div>
+      <div className="footerForm">
+        <p className="parrafoLogin">
+          Already have an account? <Link to="/login">Login Here</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
