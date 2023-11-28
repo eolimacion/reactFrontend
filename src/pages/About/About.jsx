c0d6f77b7b01bdbb51fba6e325afa11d1bb88a48
abelgh91 <abelgh91@gmail.com>
2023-11-28 15:39:28 +0100

    about us

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,6 +2,30 @@ import './About.css'
 
 export const About = () => {
   return (
-    <div>About</div>
+    <>
+      <h1 className="titleAbout">ABOUT US: </h1>
+      <div className="divAbout">
+        <p className="textAbout1">
+        Somos un equipo apasionado que ha creado esta aplicación para unir a los amantes del fútbol, MotoGP y powerlifting en un solo lugar. 
+        Nos esforzamos por ofrecer una experiencia única y personalizada para cada usuario.
+        </p>
+        <h3 className="titleMision">Misión: </h3>
+        <p className="textMision">
+        Facilitar el acceso a la emoción de los deportes, permitiendo a los usuarios crear, compartir y celebrar sus momentos favoritos en fútbol, MotoGP y powerlifting.
+        </p>
+        <h3 className="titleComunity">Compromiso con la Comunidad: </h3>
+        <p className="textComunity">Valoramos la conexión entre los aficionados. Nuestra plataforma no solo es un espacio para seguir eventos deportivos, sino también para construir comunidades sólidas donde la pasión se comparte y celebra.</p>
+        <h3 className="titleTransparency">Transparencia y Autenticidad:</h3>
+        <p className="textTransparency">
+        Creemos en la transparencia y autenticidad en cada interacción. 
+        Trabajamos incansablemente para ofrecer una aplicación que refleje nuestra dedicación a los deportes y la satisfacción del usuario.
+        </p>
+        <h3 className="titleDiscover">Descubre más:</h3>
+        <p className="textDiscover">
+        Explora la diversidad de nuestra aplicación y únete a nosotros en este viaje emocionante. 
+        ¡Conviértete en parte de nuestra comunidad deportiva hoy mismo!
+        </p>
+      </div>
+    </>
   )
 }
