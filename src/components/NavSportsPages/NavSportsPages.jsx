bf963b6b5f5a5362da199f0fddcad6577093a2af
Alsamu92 <alexsanzm@email.com>
2023-11-29 20:23:43 +0100

    css dashboards 2

--- src/components/NavSportsPages/NavSportsPages.jsx
+++ src/components/NavSportsPages/NavSportsPages.jsx
@@ -7,22 +7,33 @@ export const NavSportsPages = ({ sport }) => {
         case "powerlifting":
             return(
                 <div className='navProfile'>
-
-                <NavLink to='/powerlifting/lifters'>
-                <div className='titleNavProfile titleNav'>Lifters</div>
-                </NavLink>
-        
-                <NavLink to='/powerlifting/weight'>
-                <div className='titleNavProfile titleNav'>Weight Categories</div>
-                </NavLink>
-        
+                <div className="imageContainer">
+                    <h1 className="dashboardTitle">
+                        Know about best Lifters or PL categories
+                    </h1>
+                    <NavLink to='/powerlifting/lifters'>
+                        <img
+                            src="https://res.cloudinary.com/djfkchzyq/image/upload/v1701285556/jr6rqujwgigc0luavdb1.jpg"
+                            alt="foto moto portada"
+                            className='imageNav der'
+                        />
+                    </NavLink>
+    
+                    <NavLink to='/powerlifting/weight'>
+                        <img
+                            src="https://res.cloudinary.com/djfkchzyq/image/upload/v1701285183/gwpmadtn8ryztmczqscx.jpg"
+                            alt="foto circuito portada"
+                            className='imageNav izq'
+                        />
+                    </NavLink>
+                </div>
             </div>
             );
             case "motogp":
                 return(
                     <div className='navProfile'>
     <div className="imageContainer">
-
+<h1 className="dashboardTitle">CHOOSE YOUR ROUTE, RIDERS OR CIRCUITS</h1>
                     <NavLink to='/motogp/riders'>
                     <img src="https://res.cloudinary.com/djfkchzyq/image/upload/v1701278876/mgvyrsagdezjv9x055x5.jpg" alt="foto moto portada" className='imageNav der'></img>
                     </NavLink>
@@ -58,3 +69,4 @@ export const NavSportsPages = ({ sport }) => {
 
 
 }
+
