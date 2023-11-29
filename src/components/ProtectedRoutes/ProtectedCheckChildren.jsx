062ea7e4f945fc5e308df1fa9e704bfeb24851dd
lemonlux <sololucia17@gmail.com>
2023-11-29 11:26:19 +0100

    fixed error routes

--- src/components/ProtectedRoutes/ProtectedCheckChildren.jsx
+++ src/components/ProtectedRoutes/ProtectedCheckChildren.jsx
@@ -3,7 +3,9 @@ import { useAuth } from "../../context/authContext";
 
 export const ProtectedCheckChildren = ({children}) => {
     const {allUser, user} = useAuth(); //nos traemos el estado del usuario tanto si ha entrado por el register(allUser) como por el login(user)
-    if (allUser?.data?.user?.check == true || user.check == true){
+    console.log(allUser)
+    
+    if (allUser?.data?.user?.check == true || user?.check == true){
         return <Navigate to="/dashboard"/>;
     }
     if (user == null && allUser?.data?.confirmationCode === ""){ //si no hay usuario o esta en el estado inicial por haber recargado la pag de checkCode pues lo llevamos al login
