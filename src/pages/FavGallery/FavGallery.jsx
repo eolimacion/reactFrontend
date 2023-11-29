452c03dee985fa9aac4f7cf2c6d66fde19405388
marionovaro <marionovaro04@gmail.com>
2023-11-29 10:40:18 +0100

    cambios mario

--- src/pages/FavGallery/FavGallery.jsx
+++ src/pages/FavGallery/FavGallery.jsx
@@ -4,33 +4,40 @@ import { useEffect, useState } from "react"
 
 export const FavGallery = () => {
   const { user } = useAuth()
-  const [ show, setShow ] = useState(false)
+  const [ showDiv, setShowDiv ] = useState("")
   const [ controller, setController ] = useState()
 
   const pathName = window.location.pathname
   const sportFavName = pathName.slice(20)
 
+  // useEffect(()=>{
+  //   setShowDiv("")
+  // }, [showDiv])
+
   //!FIX ---------------------------- 
 
-  switch (sportFavName) {
-    case "FIFA":
-      const modelosFifa = document.getElementById("displayFavFifaModels")
-      modelosFifa.style.visibility = "visible"
-      break;
+  // switch (sportFavName) {
+  //   case "FIFA":
+  //     // const modelosFifa = document.getElementById("displayFavFifaModels")
+  //     // modelosFifa.style.visibility = "visible"
+  //     setShowDiv("fifa")
+  //     break;
 
-    case "MotoGP":
-      const modelosMotoGp = document.getElementById("displayFavMotoGpModels")
-      modelosMotoGp.style.visibility = "visible"
-      break;
+  //   case "MotoGP":
+  //     // const modelosMotoGp = document.getElementById("displayFavMotoGpModels")
+  //     // modelosMotoGp.style.visibility = "visible"
+  //     setShowDiv("motogp")
+  //     break;
   
-    case "PowerLifting":
-      const modelosPowerLifting = document.getElementById("displayFavPowerLiftingModels")
-      modelosPowerLifting.style.visibility = "visible"
-      break;
+  //   case "PowerLifting":
+  //     // const modelosPowerLifting = document.getElementById("displayFavPowerLiftingModels")
+  //     // modelosPowerLifting.style.visibility = "visible"
+  //     setShowDiv("powerlifting")
+  //     break;
 
-    default:
-      break;
-  }
+  //   default:
+  //     break;
+  // }
 
   const setControllerFav = (nombreController) => {
     console.log("me ejecuto")
@@ -73,31 +80,29 @@ export const FavGallery = () => {
         console.log("no hay controller")
         break;
     }
-    console.log(show)
-    setShow(true)
-    console.log(show)
   }
 
   return (
     <>
       <h1>{user.user}'s {sportFavName}'s Favorites</h1>
-      <div id = "displayFavFifaModels" style={{visibility: "hidden"}}>
+      <div id = "displayFavFifaModels">
         <button onClick={(e) => setControllerFav(e.target.innerText)}>Players</button>
         <button onClick={(e) => setControllerFav(e.target.innerText)}>Teams</button>
         <button onClick={(e) => setControllerFav(e.target.innerText)}>Elevens</button>
       </div>
-      <div id = "displayFavMotoGpModels" style={{visibility: "hidden"}}>
+      {showDiv == "motogp" && 
+      <div id = "displayFavMotoGpModels">
         <button onClick={(e) => setControllerFav(e.target.innerText)}>Riders</button>
         <button onClick={(e) => setControllerFav(e.target.innerText)}>Circuits</button>
         <button onClick={(e) => setControllerFav(e.target.innerText)}>Podiums</button>
-      </div>
-      <div id = "displayFavPowerLiftingModels" style={{visibility: "hidden"}}>
+      </div>}
+      {showDiv == "powerlifting" && <div id = "displayFavPowerLiftingModels">
         <button onClick={(e) => setControllerFav(e.target.innerText)}>Lifters</button>
         <button onClick={(e) => setControllerFav(e.target.innerText)}>Categories</button>
-      </div>
+      </div>}
 
       <section className="favGallery">
-        {show ? <CardPlayer controller={controller}/> : null}
+        <CardPlayer controller={controller}/>
       </section>
     </>
   )
