28e71abe20b6547f83cd848e7c7f44c3c26394ee
abelgh91 <abelgh91@gmail.com>
2023-11-29 10:41:19 +0100

    rutas protegidas

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -147,7 +147,6 @@ export const router = createBrowserRouter([
           },
           {
             path: "/profile/favourites",
-<<<<<<< HEAD
             element: 
             (<Protected>
               <FavoritesNav/>
@@ -162,13 +161,6 @@ export const router = createBrowserRouter([
               },
 
             ]
-=======
-            element: <FavoritesNav/>,
-          },
-          {
-            path: "/profile/favourites/:sport",
-            element: <FavGallery />,
->>>>>>> ef21eee2d4a367526d5796b3159970e11f60a154
           },
           {
             path: "/profile/edit",
