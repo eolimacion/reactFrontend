290580d6b47104c756b6e1a55899b615606c6e05
abelgh91 <abelgh91@gmail.com>
2023-11-30 19:21:19 +0100

    css podio

--- src/components/PodiumContainer/PodiumContainer.jsx
+++ src/components/PodiumContainer/PodiumContainer.jsx
@@ -27,15 +27,29 @@ import { CardInTheGallery } from "../CardInTheGallery/CardInTheGallery";
       }, [allPodiums]);
 
     return (
-      <div className="podiumAbel">
-        {allPodiums && allPodiums?.data?.map((item) => {
-            return (<div className="podiumCard" key={item._id}>{item?.name} 
-            <CardPodiums name={item?.firstPlace?.name} image={item?.firstPlace?.image}/>
-            <CardPodiums name={item?.secondPlace?.name} image={item?.secondPlace?.image}/>
-            <CardPodiums name={item?.thirdPlace?.name} image={item?.thirdPlace?.image}/>
-            {/* <button onClick={handleComment(item._id)}>comment here</button> */}
-            </div>)
-        })}
+        <div className="podiumAbel">
+        {allPodiums &&
+          allPodiums?.data?.map((item) => (
+            <div className="podiumCardContainer" key={item._id}>
+              <div className="podiumCard">{item?.name}</div>
+              <CardPodiums name={item?.firstPlace?.name} image={item?.firstPlace?.image} />
+              <CardPodiums name={item?.secondPlace?.name} image={item?.secondPlace?.image} />
+              <CardPodiums name={item?.thirdPlace?.name} image={item?.thirdPlace?.image} />
+              {/* <button onClick={handleComment(item._id)}>comment here</button> */}
+            </div>
+          ))}
       </div>
     )
   }
+
+
+//   <div className="podiumAbel">
+//         {allPodiums && allPodiums?.data?.map((item) => {
+//             return (<div className="podiumCard" key={item._id}>{item?.name} 
+//             <CardPodiums name={item?.firstPlace?.name} image={item?.firstPlace?.image}/>
+//             <CardPodiums name={item?.secondPlace?.name} image={item?.secondPlace?.image}/>
+//             <CardPodiums name={item?.thirdPlace?.name} image={item?.thirdPlace?.image}/>
+//             {/* <button onClick={handleComment(item._id)}>comment here</button> */}
+//             </div>)
+//         })}
+//       </div>
