ebd1ca9de28e007e56f95d376a86f691e940b1da
Alsamu92 <alexsanzm@email.com>
2023-11-30 11:03:20 +0100

    formulario implementado

--- src/pages/MotoGP/Riders/Riders.jsx
+++ src/pages/MotoGP/Riders/Riders.jsx
@@ -1,37 +1,46 @@
 import React, { useState } from 'react';
 import './Riders.css';
+import { FormRiders } from '../../../components';
 
-export const Riders=()=> {
+export const Riders = () => {
   const [data, setData] = useState(null);
   const [galleryLoading, setGalleryLoading] = useState(false);
   const [mainLoading, setMainLoading] = useState(false);
-  
-    return (
-      <div className="Allpage">
+  const [showForm, setShowForm] = useState(false); // Nuevo estado para controlar la visualización del formulario
+
+  const handleButtonClick = () => {
+    // Cambia el estado para mostrar u ocultar el formulario al hacer clic en el botón
+    setShowForm(!showForm);
+  };
+
+  return (
+   <div className="Allpage">
       {galleryLoading ? (
         <p>Cargando la galería...</p>
       ) : (
         <div className="galeriaPreview">
-          Galeria pequeña
+          Galería pequeña
         </div>
       )}
-      <div className="buscadorMario">buscador de Mario</div>
+      <div className="buscadorMario">Buscador de Mario</div>
       <section className="mainPage">
         {mainLoading ? (
           <p>Cargando...</p>
         ) : (
-          < >
+          <>
             <div className="displayImage">
-            Aqui se mostraria la galeria
+              {showForm ? <FormRiders /> : 'Aquí se mostraría la galería'}
             </div>
             <aside className="columnaEnlaces">
-              <div className="seccionColumna seccionUno">uno</div>
-              <div className="seccionColumna seccionDos">dos</div>
-              <div className="seccionColumna seccionTres">tres</div>
+              <div className="seccionColumna seccionUno">Uno</div>
+              <div className="seccionColumna seccionDos">Dos</div>
+              <div className="seccionColumna seccionTres">
+                <button onClick={handleButtonClick}>Mostrar/ocultar formulario</button>
+              </div>
             </aside>
           </>
         )}
       </section>
     </div>
-    );
-  }
+  );
+};
