f3549aaeddf47e588431702895329f9e3841bfe6
abelgh91 <abelgh91@gmail.com>
2023-11-30 19:07:34 +0100

    componetizar cards podium

--- src/components/CardPodiums/CardPodiums.jsx
+++ src/components/CardPodiums/CardPodiums.jsx
@@ -2,9 +2,9 @@ import './CardPodiums.css'
 
 export const CardPodiums = ({name, image}) => {
   return (
-    <section className="singleItemCard" key={image}>
-        <img className="singleItemImg" src={image} alt={name} />
-        <h2 className="singleItemName">{name}</h2>
+    <section className="singleItemCardPodium" key={image}>
+        <img className="singleItemImgPodium" src={image} alt={name} />
+        <h2 className="singleItemNamePodium">{name}</h2>
     </section>
   )
 }
--- src/components/PodiumContainer/PodiumContainer.jsx
+++ src/components/PodiumContainer/PodiumContainer.jsx
@@ -1,11 +1,17 @@
 import { useEffect, useState } from "react";
 import { getAllPodiums } from "../../services/podium.service";
 import { CardPodiums } from "../CardPodiums/CardPodiums";
+import { CardInTheGallery } from "../CardInTheGallery/CardInTheGallery";
 
   export const PodiumContainer = () => {
 
     const [podiumLoading, setPodiumLoading] = useState(false);
     const [allPodiums, setAllPodiums] = useState([]);
+    const [buttonComment, setButtonComment] = useState(false);
+
+    const handleComment = (id) => {
+        setButtonComment(id)
+    }
     
     const getPodiums = async () => {
         const podiumData = await getAllPodiums();
@@ -21,11 +27,13 @@ import { CardPodiums } from "../CardPodiums/CardPodiums";
       }, [allPodiums]);
 
     return (
-      <div>
+      <div className="podiumAbel">
         {allPodiums && allPodiums?.data?.map((item) => {
-            return (<div className="podiumCard" key={item._id}>{item?.name} <div className="podiumFirsPlace"><h1>{item?.firstPlace?.name}</h1><img src={item?.firstPlace?.image}/></div>
-            <div className="podiumSecondPlace"><h1>{item?.secondPlace?.name}</h1><img src={item?.secondPlace?.image}/></div>
-            <div className="podiumThirdPlace"><h1>{item?.thirdPlace?.name}</h1><img src={item?.thirdPlace?.image}/></div>
+            return (<div className="podiumCard" key={item._id}>{item?.name} 
+            <CardPodiums name={item?.firstPlace?.name} image={item?.firstPlace?.image}/>
+            <CardPodiums name={item?.secondPlace?.name} image={item?.secondPlace?.image}/>
+            <CardPodiums name={item?.thirdPlace?.name} image={item?.thirdPlace?.image}/>
+            {/* <button onClick={handleComment(item._id)}>comment here</button> */}
             </div>)
         })}
       </div>
